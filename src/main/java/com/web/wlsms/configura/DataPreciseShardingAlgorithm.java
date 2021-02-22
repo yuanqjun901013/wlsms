10ec7edcd1f3ed4b8ec27ef8c341d3031232ac4a
15090387 <15090387@suning.com>
2021-02-22 11:13:21 +0800

    20210204调整pom文件

--- src/main/java/com/web/wlsms/configura/DataPreciseShardingAlgorithm.java
+++ src/main/java/com/web/wlsms/configura/DataPreciseShardingAlgorithm.java
@@ -1,18 +1,29 @@
 package com.web.wlsms.configura;
 
-import com.web.wlsms.utils.DateUtil;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
 
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 
-public class DataPreciseShardingAlgorithm implements PreciseShardingAlgorithm<Date> {
+public class DataPreciseShardingAlgorithm implements PreciseShardingAlgorithm<String> {
     @Override
-    public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Date> preciseShardingValue) {
+    public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<String> preciseShardingValue) {
         StringBuffer tableName = new StringBuffer();
-        tableName.append(preciseShardingValue.getLogicTableName())
-                .append("_").append(DateUtil.date2Str(preciseShardingValue.getValue(), DateUtil.TIME_MONEN));
+        String timeMonth = date2Str(preciseShardingValue.getValue(), "yyyyMM");
+        tableName.append(preciseShardingValue.getLogicTableName()).append(timeMonth);
         return tableName.toString();
+    }
+
+    public static String date2Str(String date, String format) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
+        try {
+            Date dateTime = dateFormat.parse(date);
+            return sdf.format(dateTime);
+        }catch (Exception e){
+            return sdf.format(new Date());
+        }
     }
 }
--- src/main/java/com/web/wlsms/controller/data/DataController.java
+++ src/main/java/com/web/wlsms/controller/data/DataController.java
@@ -1,12 +1,15 @@
 package com.web.wlsms.controller.data;
 
 import com.github.pagehelper.PageInfo;
+import com.web.wlsms.entity.DataEntity;
 import com.web.wlsms.request.SimpleRequest;
 import com.web.wlsms.service.data.DataService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -82,6 +85,37 @@ public class DataController {
         }catch (Exception e){
             resultMap.put("total", 0);
             resultMap.put("rows", "");
+        }
+        return resultMap;
+    }
+
+    @RequestMapping("insertData")
+    public Map<String,Object> insertData(SimpleRequest params){
+        Map<String,Object> resultMap = new HashMap<>();
+        try {
+            DataEntity dataEntity = new DataEntity();
+            dataEntity.setDataValueA("1");
+            dataEntity.setDataValueB("1");
+            dataEntity.setDataValueC("1");
+            dataEntity.setDataValueD("1");
+            dataEntity.setDataValueE("1");
+            dataEntity.setDataValueF("1");
+            dataEntity.setDataValueG("1");
+            dataEntity.setDataValueH("1");
+            dataEntity.setDataValueI("1");
+            dataEntity.setDataValueJ("1");
+            dataEntity.setDataValueK("1");
+            dataEntity.setDataValueL("1");
+            dataEntity.setDataValueM("1");
+            dataEntity.setDataValueN("1");
+            dataEntity.setPositionCode("code1");
+            SimpleDateFormat spf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String createTime = spf.format(new Date());
+            dataEntity.setCreateTime(createTime);
+            dataService.insertData(dataEntity);
+            resultMap.put("data","数据添加成功");
+        }catch (Exception e){
+            resultMap.put("data","数据添加失败");
         }
         return resultMap;
     }
