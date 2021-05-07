434cd3a57eb0228ee3c741f117c9ef49ac1436cf
15090387 <15090387@suning.com>
2021-05-07 15:26:13 +0800

    20210507 优化统计图1

---
+++ src/main/java/com/web/wlsms/entity/AllParamEntity.java
@@ -0,0 +1,13 @@
+package com.web.wlsms.entity;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class AllParamEntity {
+    private String name;
+    private long value;
+}
